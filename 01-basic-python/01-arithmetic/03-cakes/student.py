ad48523fdfffee650353aa1fc8523a84a35017f5
Nathan Perron <nathan.perron@student.ucll.be>
2024-02-20 11:02:52 +0100

    deel basics

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,9 @@
-# Write your code here
+def cakes(eggs, butter, flour):
+    ingredients_list = {"eggs": 5, "butter": 250, "flour": 250}
+
+    total_cakes = min(
+        eggs // ingredients_list["eggs"],
+        butter // ingredients_list["butter"],
+        flour // ingredients_list["flour"],
+    )
+    return total_cakes
